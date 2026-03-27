b698aa9104d4b93c33d60652e7eafc9682808ef9
HalinaAmorim <105753612+HalinaAmorim@users.noreply.github.com>
2026-03-27 16:52:57 -0300

    feat: implementa layouts público e autenticado

--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -1,60 +1,47 @@
+import { useAuth } from '@/features/auth/contexts/useAuth'
+import Footer from '@/shared/components/footer'
+import NavBar from '@/shared/components/navbar'
+import { Button } from '@/shared/components/ui/button'
+import { useScroll } from '@/shared/hooks/useScroll'
+import { Link, Navigate, Outlet } from 'react-router'
 
+function PublicLayout() {
+  const { isAuthenticated } = useAuth()
+  useScroll()
 
+  if (isAuthenticated) {
+    return <Navigate to="/feed" replace />
+  }
 
+  return (
+    <>
+      <div className="flex flex-col min-h-svh">
+        <NavBar>
+          <NavBar.Brand to="/" />
 
+          <NavBar.Links>
+            <NavBar.Link to="/" text="Início" />
+            <NavBar.Link to="#feature-section" text="Recursos" />
+            <NavBar.Link to="#faq-section" text="Perguntas Frequentes" />
+          </NavBar.Links>
 
+          <NavBar.Actions>
+            <Button variant="ghost" size="lg" asChild>
+              <Link to="/login">Entrar</Link>
+            </Button>
 
+            <Button size="lg" asChild>
+              <Link to="/register">Criar Conta</Link>
+            </Button>
+          </NavBar.Actions>
+        </NavBar>
+        <main className="flex-1 flex flex-col">
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </>
+  )
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default PublicLayout
